537c587de594ff759e7d32a30d3038a4bc21cbcc
Diego Inácio <diegodci@gmail.com>
2021-01-28 18:45:52 -0300

    Upload page assets template

---
+++ docs/assets/template/header.js
@@ -0,0 +1,6 @@
+document.querySelector("header#header.alt").innerHTML = `
+<h1><a href="index.html">⚙️ ${window.PAGE_TITLE}</a></h1>
+<nav>
+  <a href="#menu">Menu</a>
+</nav>
+`;
---
+++ docs/assets/template/menu.js
@@ -0,0 +1,13 @@
+document.querySelector("nav#menu").innerHTML = `
+<div class="inner">
+  <h2>Menu</h2>
+  <ul class="links">
+    <li><a href="index.html">Home</a></li>
+    <li><a href="maya.html">Autodesk <span class="software">Maya</span></a></li>
+    <li><a href="mari.html">Foundry <span class="software">Mari</span></a></li>
+    <li><a href="nuke.html">Foundry <span class="software">Nuke</span></a></li>
+    <li><a href="houdini.html">SideFX <span class="software">Houdini</span></a></li>
+  </ul>
+  <a href="#" class="close">Close</a>
+</div>
+`;
